16774ed07335c63aafb904383d8781b57d9d7e61
talksik <patel.arjun50@gmail.com>
2022-05-27 20:20:55 -0500

    adding in main electron app with react

---
+++ packages/desktop/src/app.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.body as HTMLElement);
+
+root.render(
+    <React.StrictMode>
+      hello from react
+    </React.StrictMode>
+  );
