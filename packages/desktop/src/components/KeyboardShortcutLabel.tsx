57fd2c997b9c6a96a42c8262f49046703a44dced
talksik <patel.arjun50@gmail.com>
2022-05-28 02:24:06 -0500

    Update KeyboardShortcutLabel.tsx

--- packages/desktop/src/components/KeyboardShortcutLabel.tsx
+++ packages/desktop/src/components/KeyboardShortcutLabel.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box, Paper, Typography } from '@mui/material';
 import { blueGrey } from '@mui/material/colors';
 
 export default function KeyboardShortcutLabel({ label }: { label: string }) {
   return (
-    <Box
+    <Paper
+      elevation={0}
+      variant={'outlined'}
       sx={{
         px: 1,
         py: 0.25,
         border: `1px solid`,
         bgcolor: blueGrey[50],
-        boxShadow: 1,
         color: blueGrey[300],
       }}
     >
       <Typography variant="caption">{label}</Typography>
-    </Box>
+    </Paper>
   );
 }
