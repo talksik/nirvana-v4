b1bf26d7b180edcc43a9e875af64bc8d5b77dc59
talksik <patel.arjun50@gmail.com>
2022-05-30 10:55:45 -0500

    nice adjustment of nirvana team use

--- packages/desktop/src/util/support.ts
+++ packages/desktop/src/util/support.ts
@@ -1,4 +1,4 @@
 /**
  * The string that should be used to search for the support account, regardless of the environment
  */
-export const SUPPORT_DISPLAY_NAME = 'Nirvana Support';
+export const SUPPORT_DISPLAY_NAME = 'Nirvana | Team';
