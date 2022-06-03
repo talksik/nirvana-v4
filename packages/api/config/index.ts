6eb809286427e081e810ab04e8d37a9fcadbe2bf
talksik <patel.arjun50@gmail.com>
2022-06-03 13:30:06 -0700

    adding env variables and morgan and sentry

---
+++ packages/api/config/index.ts
@@ -0,0 +1,27 @@
+interface EnvironmentConfig {
+  MONGO_CONNECTION_STRING: string;
+  JWT_TOKEN_SECRET: string;
+  GOOGLE_AUTH_CLIENT_ID: string;
+}
+
+// keep private
+const getEnvironmentVariables = (): EnvironmentConfig => {
+  // TODO
+  if (process.env.NODE_ENV === 'production') {
+    //
+  }
+
+  if (process.env.NODE_ENV === 'development') {
+    //
+  }
+
+  return {
+    GOOGLE_AUTH_CLIENT_ID:
+      '423533244953-banligobgbof8hg89i6cr1l7u0p7c2pk.apps.googleusercontent.com',
+    JWT_TOKEN_SECRET: 'afajdslfwk1@lkkasdfl21ASDF!2',
+    MONGO_CONNECTION_STRING:
+      'mongodb+srv://default:M9iZXokJlZpN4KLX@cluster0.mkuqa.mongodb.net/default?retryWrites=true&w=majority',
+  };
+};
+
+export const environmentVariables: EnvironmentConfig = getEnvironmentVariables();
--- packages/api/index.ts
+++ packages/api/index.ts
@@ -1,24 +1,39 @@
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
+
+/* eslint-disable @typescript-eslint/no-var-requires */
 import express, { Application, NextFunction, Request, Response } from 'express';
 
 import cors from 'cors';
+import morgan from 'morgan';
 
 const app = express();
 
+app.use(morgan('combined'));
 app.use(cors());
 app.use(express.json());
-
 app.use((req: Request, res: Response, next: NextFunction) => {
   console.log('Time: ', new Date());
   next();
 });
-
 app.get('/', (req: Request, res: Response) => {
   res.send('hello world.');
 });
-
 app.use('/api/status', (req: Request, res: Response) => {
   res.json({ message: 'wohoo, server is healthy' });
 });
 
 const PORT = process.env.PORT || 8080;
-const server = app.listen(PORT, () => console.log('express running'));
+const server = app.listen(PORT, () =>
+  console.log(`express running on port | ${PORT} in host machine`),
+);
+
+// won't catch any errors here and we don't want it to
+console.log();
+
+require('socket.io')(server, {
+  // todo: add authentication
+  cors: {
+    origin: '*',
+  },
+});
