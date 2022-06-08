7aab1ee26cfa5a6a8aff67a94bd76cfbc41c9ed6
talksik <patel.arjun50@gmail.com>
2022-06-08 05:28:38 -0500

    adding global error handlers

--- packages/api/index.ts
+++ packages/api/index.ts
@@ -16,10 +16,20 @@ app.use(morgan('combined'));
 app.use(cors());
 app.use(express.json());
 
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err);
+  next(err);
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500);
+  res.render('error', { error: err });
+});
+
 app.get('/', (req: Request, res: Response) => {
   res.send('hello world.');
 });
-app.use('/api/status', (req: Request, res: Response) => {
+app.get('/api/status', (req: Request, res: Response) => {
   res.json({ message: 'wohoo, server is healthy' });
 });
 
@@ -27,9 +37,6 @@ const PORT = process.env.PORT || 8080;
 const server = app.listen(PORT, () =>
   console.log(`express running on port | ${PORT} in host machine`),
 );
-
-// won't catch any errors here and we don't want it to
-console.log();
 
 const io = require('socket.io')(server, {
   // todo: add authentication
